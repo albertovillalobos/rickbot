6ee1765fdb25e9e487c6788eac3c66dff4b2d24b
Alberto Villalobos <avillalobos@invoca.com>
2016-04-04 13:41:55 -0700

    add sample config

---
+++ config.sample.js
@@ -0,0 +1,5 @@
+var config = {};
+config.webhookurl = 'https://hooks.slack.com/services/xxxxxxxxx/xxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxx'; //slackbot you want
+config.jenkinstein = 'http://<url>/<endpoint>'; // jenkinstain url
+
+module.exports = config;
