cfcd0c6e4f6dd5244dcc59ff87763d9e81975ade
Nick Burwell <nburwell@gmail.com>
2016-04-06 22:45:48 -0700

    Further DRYing up rickbot main code by sharing the request and error logic across green and red

--- rickbot.js
+++ rickbot.js
@@ -1,7 +1,6 @@
 var request = require('request');
 var config = require('./config');
 var PayloadTemplate = require('./payloadTemplate');
-
 var timerInterval = 5000; // how long between checks?
 
 
@@ -12,14 +11,13 @@ var timeStamp = function() {
 // new greeting
 console.log(timeStamp(),'Rickbot online!');
 
-
-
 var productionGreen = true;
 var masterGreen = true;
 var productionBuild = {};
 var masterBuild = {};
 
-var payload = new PayloadTemplate();
+var payloadTemplate = new PayloadTemplate();
+var currentPayload;
 
 setInterval( function() {
   console.log(timeStamp(),'checking...');
@@ -36,93 +34,71 @@ setInterval( function() {
         return build.name === 'production';
       })[0];
     } catch(e) {
-      console.log(e);
-      console.log(body);
+      console.log(e, body);
       return;
     }
 
+    currentPayload = null;
+    
     // productionBuild.green = false; // testing
     if (productionGreen && !productionBuild.green) {
       console.log(timeStamp(),"Production has been broken");
+      currentPayload = payloadTemplate.red(productionBuild);
+    } else if (!productionGreen && productionBuild.green) {
+      currentPayload = payloadTemplate.green(productionBuild);
+    }
+    
+    if (currentPayload) {
       request({
-          url: config.webhookurl,
-          method: 'POST',
-          body: JSON.stringify(payload.red(productionBuild))
+        url: config.webhookurl,
+        method: 'POST',
+        body: JSON.stringify(currentPayload)
       }, function(error, response, body){
-          if(error) {
-              console.log(timeStamp(),'Error posting to slack after prod broken',error);
-          } else {
-              console.log(timeStamp(),response.statusCode, body);
-          }
+        if (error) {
+          console.log(timeStamp(), 'Error posting to slack: ', error, currentPayload);
+        } else {
+          console.log(timeStamp(), response.statusCode, body);
+        }
       });
     }
-
-    if (!productionGreen && productionBuild.green) {
-      console.log(timeStamp(),"Production has been fixed");
-      request({
-          url: config.webhookurl,
-          method: 'POST',
-          body: JSON.stringify(payload.green(productionBuild))
-      }, function(error, response, body){
-          if(error) {
-              console.log(timeStamp(),'Error posting to slack after prod fix',error);
-          } else {
-              console.log(timeStamp(),response.statusCode, body);
-          }
-      });
-    }
-
+    
     productionGreen = productionBuild.green;
-
-
 
 
     // ==========
     // Master
     // ==========
-
-
+    
+    currentPayload = null;
 
     try {
       masterBuild = JSON.parse(body).latest.filter(function(value) {
         return value.name === 'master';
       })[0];
     } catch(e) {
-      console.log(e);
-      console.log(body);
+      console.log(e, body);
       return;
     }
-
-
 
     // masterBuild.green = false; //testing
     if (masterGreen && !masterBuild.green) {
       console.log(timeStamp(),'masters on fire');
-      request({
-          url: config.webhookurl,
-          method: 'POST',
-          body: JSON.stringify(payload.red(masterBuild))
-      }, function(error, response, body){
-          if(error) {
-              console.log(timeStamp(),'Error posting to slack on master broken',error);
-          } else {
-              console.log(timeStamp(),response.statusCode, body);
-          }
-      });
-
+      currentPayload = payloadTemplate.red(masterBuild);
+    } else if (!masterGreen && masterBuild.green) {
+      currentPayload = payloadTemplate.green(masterBuild);
     }
-    if (!masterGreen && masterBuild.green) {
-      console.log(timeStamp(),'masters fixed!');
+    
+    if (currentPayload) {
       request({
-          url: config.webhookurl,
-          method: 'POST',
-          body: JSON.stringify(payload.green(masterBuild))
+        url: config.webhookurl,
+        method: 'POST',
+        body: JSON.stringify(currentPayload)
       }, function(error, response, body){
-          if(error) {
-              console.log(timeStamp(),'Error posting to slack on master fixed',error);
-          } else {
-              console.log(timeStamp(),response.statusCode, body);
-          }
+        if(error) {
+          console.log(timeStamp(),'Error posting to slack: ', error, currentPayload);
+        } else {
+          console.log(timeStamp(),response.statusCode, body);
+        }
       });
     }
 
